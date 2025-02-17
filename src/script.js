7607eb2ebbd963d9bb7f4b97b303a928f2ada78d
Jada Baron <jadachantellebaron@gmail.com>
2025-02-17 18:35:12 +0000

    Make computer choice variable name more specific

--- src/script.js
+++ src/script.js
@@ -10,12 +10,12 @@ const paper = "Paper";
 const scissors = "Scissors";
 
 function getComputerChoice() {
-  let selectedOption = Math.random();
-  if (selectedOption <= 0.3) {
+  let computerChoice = Math.random();
+  if (computerChoice <= 0.3) {
     return rock;
-  } else if (selectedOption > 0.3 && selectedOption <= 0.6) {
+  } else if (computerChoice > 0.3 && computerChoice <= 0.6) {
     return paper;
-  } else if (selectedOption > 0.6 && selectedOption <= 1) {
+  } else if (computerChoice > 0.6 && computerChoice <= 1) {
     return scissors;
   }
 }
