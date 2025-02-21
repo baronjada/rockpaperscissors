0592cc5646b369e992bc5f0dd9d21f8ca7d75773
Jada Baron <jadachantellebaron@gmail.com>
2025-02-21 17:20:01 +0000

    Add functionality to play 5 rounds and alert game winner
    Built a for loop inside the playGame function to run the game for 5 rounds and track sores then if else statements to alert winner at the end.

--- src/script.js
+++ src/script.js
@@ -1,11 +1,3 @@
-/*
-Computer logic:
-1. Create new function named getComputerChoice - done
-2. getComputerChoice to randomly return one of following string values = "rock", "paper", "scissors" - done
-3. Use Math.random method to return random number - done
-4. Use conditions to return one of the multiple string values - done
-5. Use console.log to test function - done
-*/
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
@@ -19,13 +11,6 @@ function getComputerChoice(compChoice) {
     return scissors;
   }
 }
-
-/*
-Human logic:
-1. Create new function named getHumanChoice - done
-2. Create prompt to get user imput - done
-3. Console.log user input - done
-*/
 
 function getHumanChoice() {
   let humanChoiceInput = prompt(
@@ -47,68 +32,62 @@ function getHumanChoice() {
   return humanChoiceInput;
 }
 
-/*Declare the players score variables:
-1. Create a variable named humanScore - done
-2. Create a variable named computerScore - done
-3. Initialise those two variables with a 0 - done
-*/
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-let humanScore = 0;
-let computerScore = 0;
+  function playRound(humanChoice, computerChoice) {
+    switch (true) {
+      case humanChoice == computerChoice:
+        console.log("You drew the same thing! No winners here.");
+        break;
+      case humanChoice == paper && computerChoice == scissors:
+        console.log("Computer wins! Scissors beats paper.");
+        computerScore++;
+        break;
+      case humanChoice == scissors && computerChoice == paper:
+        console.log("You win! Scissors beats paper.");
+        humanScore++;
+        break;
+      case humanChoice == rock && computerChoice == paper:
+        console.log("Computer wins! Paper beats rock.");
+        computerScore++;
+        break;
+      case humanChoice == paper && computerChoice == rock:
+        console.log("You win! Paper beats rock.");
+        humanScore++;
+        break;
+      case humanChoice == scissors && computerChoice == rock:
+        console.log("Computer wins! Rock beats scissors.");
+        computerScore++;
+        break;
+      case humanChoice == rock && computerChoice == scissors:
+        console.log("You win! Rock beats scissors.");
+        humanScore++;
+        break;
+      default:
+        console.log("Not quite sure who wins here...");
+    }
+  }
 
-/*Write the logic to play a single round
-1. Create a new function named playRound - done
-2. Define two parameters for playRound: 
-humanChoice and computerChoice - done
-3. Make your function's humanChoice parameter case insensitive
-    so that players can input "rock", "ROCK", "RocK",
-    or other variations. - done
-  4. Write the code for your playRound function to console.log
-  a string value representing the round winner, such as
-  "You lose! Paper beats rock". - done
-  5. Increment the humanScore or computerScore variable
-   based on the round winner. - done
-*/
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice(Math.random());
 
-function playRound(humanChoice, computerChoice) {
-  switch (true) {
-    case humanChoice == computerChoice:
-      console.log("You drew the same thing! No winners here.");
-      break;
-    case humanChoice == paper && computerChoice == scissors:
-      console.log("Computer wins! Scissors beats paper.");
-      computerScore++;
-      break;
-    case humanChoice == scissors && computerChoice == paper:
-      console.log("You win! Scissors beats paper.");
-      humanScore++;
-      break;
-    case humanChoice == rock && computerChoice == paper:
-      console.log("Computer wins! Paper beats rock.");
-      computerScore++;
-      break;
-    case humanChoice == paper && computerChoice == rock:
-      console.log("You win! Paper beats rock.");
-      humanScore++;
-      break;
-    case humanChoice == scissors && computerChoice == rock:
-      console.log("Computer wins! Rock beats scissors.");
-      computerScore++;
-      break;
-    case humanChoice == rock && computerChoice == scissors:
-      console.log("You win! Rock beats scissors.");
-      humanScore++;
-      break;
-    default:
-      console.log("Not quite sure who wins here...");
+    playRound(humanSelection, computerSelection);
+
+    console.log(
+      `You have ${humanScore} point(s) and the computer has ${computerScore} point(s).`
+    );
+  }
+
+  if (humanScore > computerScore) {
+    alert("You won this game!");
+  } else if (computerScore > humanScore) {
+    alert("Unlucky, the computer wins this game!");
+  } else {
+    alert("It's a draw!");
   }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice(Math.random());
-
-playRound(humanSelection, computerSelection);
-
-console.log(
-  `In case you were curious... you picked ${humanSelection} and the computer picked ${computerSelection}. You have ${humanScore} points and the computer has ${computerScore} points.`
-);
+playGame();
