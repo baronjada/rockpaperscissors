ffb21c8095fe3ce76b2be95c77ded04c2524e7bc
Jada Baron <jadachantellebaron@gmail.com>
2025-04-23 19:09:37 +0100

    Re-factor logic for gameplay using user click event
    Added event listener to user button choices and added data attribute to log user choice and be used in click event function. Computer choice logic remained mostly the same with random number generator and switch statement. Refactored console log to track player choices and winner.

--- src/script.js
+++ src/script.js
@@ -1,82 +1,50 @@
-function getComputerChoice(compChoice) {
-  if (compChoice <= 0.3) {
+const buttons = document.querySelectorAll(".user-rps-buttons");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const humanChoice = e.target.dataset.choice;
+    const computerChoice = getComputerChoice(Math.random());
+    playGame(humanChoice, computerChoice);
+  });
+});
+
+function getComputerChoice(num) {
+  if (num <= 0.3) {
     return "rock";
-  } else if (compChoice > 0.3 && compChoice <= 0.6) {
+  } else if (num <= 0.6) {
     return "paper";
-  } else if (compChoice > 0.6 && compChoice <= 1) {
+  } else {
     return "scissors";
   }
 }
 
-function getHumanChoice() {
-  let humanChoice;
+function playGame(humanChoice, computerChoice) {
+  console.log(`You chose ${humanChoice}`);
+  console.log(`Computer chose ${computerChoice}`);
 
-  const userRockButton = document.querySelector("#user-rock-button");
-  const userPaperButton = document.querySelector("#user-paper-button");
-  const userScissorsButton = document.querySelector("#user-scissors-button");
-
-  userRockButton.addEventListener("click", () => {
-    humanChoice = "rock";
-    return humanChoice;
-  });
-
-  userPaperButton.addEventListener("click", () => {
-    humanChoice = "paper";
-    return humanChoice;
-  });
-
-  userScissorsButton.addEventListener("click", () => {
-    humanChoice = "scissors";
-    return humanChoice;
-  });
-}
-
-function playGame() {
-  function playRound(humanChoice, computerChoice) {
-    switch (true) {
-      case humanChoice === computerChoice:
-        console.log("You drew the same thing! No winners here.");
-        break;
-      case humanChoice === "paper" && computerChoice === "scissors":
-        console.log("Computer wins! Scissors beats paper.");
-        // computerScore++;
-        break;
-      case humanChoice === "scissors" && computerChoice === "paper":
-        console.log("You win! Scissors beats paper.");
-        // humanScore++;
-        break;
-      case humanChoice === "rock" && computerChoice === "paper":
-        console.log("Computer wins! Paper beats rock.");
-        // computerScore++;
-        break;
-      case humanChoice === "paper" && computerChoice === "rock":
-        console.log("You win! Paper beats rock.");
-        // humanScore++;
-        break;
-      case humanChoice === "scissors" && computerChoice === "rock":
-        console.log("Computer wins! Rock beats scissors.");
-        // computerScore++;
-        break;
-      case humanChoice === "rock" && computerChoice === "scissors":
-        console.log("You win! Rock beats scissors.");
-        // humanScore++;
-        break;
-      default:
-        console.log("Not quite sure who wins here...");
-    }
+  switch (true) {
+    case humanChoice === computerChoice:
+      console.log("You drew the same thing! No winners here.");
+      break;
+    case humanChoice === "paper" && computerChoice === "scissors":
+      console.log("Computer wins! Scissors beats paper.");
+      break;
+    case humanChoice === "scissors" && computerChoice === "paper":
+      console.log("You win! Scissors beats paper.");
+      break;
+    case humanChoice === "rock" && computerChoice === "paper":
+      console.log("Computer wins! Paper beats rock.");
+      break;
+    case humanChoice === "paper" && computerChoice === "rock":
+      console.log("You win! Paper beats rock.");
+      break;
+    case humanChoice === "scissors" && computerChoice === "rock":
+      console.log("Computer wins! Rock beats scissors.");
+      break;
+    case humanChoice === "rock" && computerChoice === "scissors":
+      console.log("You win! Rock beats scissors.");
+      break;
+    default:
+      console.log("Not quite sure who wins here...");
   }
-
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice(Math.random());
-
-  console.log(
-    "this is what the user selection and computer selection are coming out as when the playRound function is called",
-    playRound(humanSelection, computerSelection)
-  );
 }
-
-const userRpsButtons = document.querySelectorAll(".user-rps-buttons");
-
-userRpsButtons.forEach((button) => {
-  button.addEventListener("click", playGame);
-});
